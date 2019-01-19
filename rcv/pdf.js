7b2ea0c094ee6693f5c365394e087a205b3a7b82
David Tulloh <git-david@tulloh.id.au>
2019-01-19 19:29:07 +1100

    Tidy up pass

--- rcv/pdf.js
+++ rcv/pdf.js
@@ -1,16 +1,15 @@
 const puppeteer = require('puppeteer');
 const { spawnSync } = require( 'child_process' );
 
+const css_shrink = '.summary > p { height: 10px; }';
+const css_body_page = '@page { margin-top: 20mm; margin-bottom: 20mm }';
+const css_first_page = '@page { margin-top: 10mm; margin-bottom: 10mm }';
+const footerTemplate = '<footer style="background: red; font-size: 16px; font-family: \"Lato\", sans-serif; font-weight: 400; position: fixed; bottom: 0; right: 0">David Tulloh &nbsp;&emdash;&nbsp; david@tulloh.id.au</footer>';
 
 (async () => {
-	  const browser = await puppeteer.launch();
-	  const page = await browser.newPage();
-	  await page.goto('http://localhost:3000/', {waitUntil: 'networkidle2'});
-
-	const css_shrink = '.summary > p { height: 10px; }';
-	const css_body_page = '@page { margin-top: 20mm; margin-bottom: 20mm }';
-	const css_first_page = '@page { margin-top: 10mm; margin-bottom: 10mm }';
-	const footerTemplate = '<footer style="background: red; font-size: 16px; font-family: \"Lato\", sans-serif; font-weight: 400; position: fixed; bottom: 0; right: 0">David Tulloh &nbsp;&emdash;&nbsp; david@tulloh.id.au</footer>';
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto('http://localhost:3000/', {waitUntil: 'networkidle2'});
 
 	await page.mainFrame().addStyleTag({content: css_first_page});
 	await page.pdf({
@@ -18,7 +17,7 @@ const { spawnSync } = require( 'child_process' );
 		format: 'A4',
 		pageRanges: '1',
 		printBackground: true,
-		displayHeaderFooter: false, // Doesn't seem to work properly
+		displayHeaderFooter: false,
 	});
 
 	await page.mainFrame().addStyleTag({content: css_shrink});
@@ -28,8 +27,8 @@ const { spawnSync } = require( 'child_process' );
 		format: 'A4',
 		pageRanges: '2-',
 		printBackground: true,
-		displayHeaderFooter: false, // Doesn't seem to work properly
-		/*
+		displayHeaderFooter: false,
+		/* Header/footer doesn't work properly, doesn't seem to style
 		headerTemplate: '<div style="background: blue"></div>',
 		footerTemplate,
 		*/
@@ -43,7 +42,7 @@ const { spawnSync } = require( 'child_process' );
 		*/
 	});
 
-	  await page.goto('http://localhost:3000/footer.html', {waitUntil: 'networkidle2'});
+	await page.goto('http://localhost:3000/footer.html', {waitUntil: 'networkidle2'});
 	await page.pdf({
 		path: 'footer.pdf',
 		format: 'A4',
@@ -52,13 +51,13 @@ const { spawnSync } = require( 'child_process' );
 		displayHeaderFooter: false,
 	});
 
-	  await browser.close();
+	await browser.close();
 
-	spawnSync( 'pdftk', [ 'body.pdf', 'background', 'footer.pdf', 'output', 'bodybg.pdf' ] );
-	spawnSync( 'pdftk', [ 'first.pdf', 'bodybg.pdf', 'cat', 'output', 'david_tulloh.pdf' ] );
+	spawnSync('pdftk', 'body.pdf background footer.pdf output bodybg.pdf'.split(' '));
+	spawnSync('pdftk', 'first.pdf bodybg.pdf cat output david_tulloh.pdf'.split(' '));
 
 	// Clean up
-	spawnSync( 'rm', [ '-f', 'first.pdf', 'body.pdf', 'footer.pdf', 'bodybg.pdf' ]);
+	spawnSync('rm', ['-f', 'first.pdf', 'body.pdf', 'footer.pdf', 'bodybg.pdf']);
 
 
 	console.log("Generated david_tulloh.pdf");
