f355463325ac51e25ca74853fe8812368e32a62a
David Tulloh <git-david@tulloh.id.au>
2019-01-22 09:51:48 +1100

    Shift to proper directory, add error management

--- scripts/pdf.js
+++ scripts/pdf.js
@@ -1,3 +1,21 @@
+'use strict';
+
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = 'production';
+process.env.NODE_ENV = 'production';
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+	if (err.message.startsWith('net::ERR_CONNECTION_REFUSED')) {
+		console.log("Must run the local server using `yarn start`.");
+		process.exit(1);
+	}
+	throw err;
+});
+
+
 const puppeteer = require('puppeteer');
 const { spawnSync } = require( 'child_process' );
 
